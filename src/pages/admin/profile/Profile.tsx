f5f82b297e368fbf787d17d2b1ecceaee7c96703
dpetrov <dpetrov@msoftgroup.ru>
2024-05-17 02:15:32 +0300

    feat: started profile

--- src/pages/admin/profile/Profile.tsx
+++ src/pages/admin/profile/Profile.tsx
@@ -1,5 +1,23 @@
+import './profile.scss';
+
 function Profile() {
-  return <></>;
+  return (
+    <div className="profile">
+      <h1>Profile</h1>
+      <div className="userInfo">
+        <img src={'https://i.pinimg.com/originals/78/73/70/787370e61c34dfbfb798ce08ac75a610.png'} className="avatar" />
+        <div className="info">
+          <div className="fioContainer">
+            <div className="fio">Daniil Petrov</div>
+          </div>
+          <div className="loginCase">
+            <div className="log">dpetrov</div>
+            <img src="/edit.svg" className="edit-btn" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Profile;
