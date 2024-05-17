208c16b64d6397b57c01214588a620b94a9a20d2
Ivosha <ivandidik1990@gmail.com>
2024-05-17 19:43:55 +0300

    Добавил кнопку

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -32,6 +32,7 @@ const SettingsMenu: React.FC<SettingsMenu> = ({ isOpen }) => {
   if (isOpen) {
     return (
       <div className="settingsMenuContainer">
+        <span className="settingsButton">Edit profile</span>
         <span className="settingsButton" onClick={() => logout()}>
           Logout
         </span>
@@ -61,7 +62,7 @@ const Navbar: React.FC<NavbarProps> = ({ setToggle, toggle, isMenuOpen, setIsMen
           <img src={isImage ? isImage : '/noavatar.png'} alt="avatar" />
           <span>{isName}</span>
         </div>
-        <img src="/settings.svg" alt="" className="icon" onClick={() => setIsMenuOpen(!isMenuOpen)} />
+        <img src="/settings.svg" alt="settings" className="icon" onClick={() => setIsMenuOpen(!isMenuOpen)} />
         <SettingsMenu isOpen={isMenuOpen} />
       </div>
     </div>
