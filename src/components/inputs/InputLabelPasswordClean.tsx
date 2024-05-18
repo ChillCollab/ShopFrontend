22419abd1a2d4c06d7d7744647eab48ace9f85ad
Daniil Petrov <dpetrov@msoftgroup.ru>
2024-05-18 03:40:20 +0300

    feat: added inputs in Profile

---
+++ src/components/inputs/InputLabelPasswordClean.tsx
@@ -0,0 +1,53 @@
+import { FormControl, InputLabel, OutlinedInput } from '@mui/material';
+import React from 'react';
+import './inputs.scss';
+
+interface InputLabelClean {
+  label:
+    | string
+    | number
+    | boolean
+    | React.ReactElement<any, string | React.JSXElementConstructor<any>>
+    | Iterable<React.ReactNode>
+    | null
+    | undefined;
+  style: React.CSSProperties | undefined;
+  onChange: React.ChangeEventHandler<HTMLInputElement | HTMLTextAreaElement> | undefined;
+  size: 'small' | 'medium' | undefined;
+  onKeyDown: React.KeyboardEventHandler<HTMLInputElement | HTMLTextAreaElement> | undefined;
+  onFocus: React.FocusEventHandler<HTMLInputElement | HTMLTextAreaElement> | undefined;
+  onClick: React.MouseEventHandler<HTMLButtonElement> | undefined;
+  value?: string;
+  disabled?: boolean;
+}
+
+export const InputLabelPasswordClean: React.FC<InputLabelClean> = ({
+  label,
+  style,
+  onChange,
+  size,
+  onKeyDown,
+  onFocus,
+  disabled,
+  value,
+}) => {
+  return (
+    <FormControl className="custom-form-control" variant="outlined">
+      <InputLabel htmlFor="outlined-adornment-password" style={{ display: 'flex', alignItems: 'center' }}>
+        {label}
+      </InputLabel>
+      <OutlinedInput
+        disabled={disabled}
+        style={style}
+        onChange={onChange}
+        size={size}
+        value={value}
+        id="outlined-adornment-password"
+        type={'password'}
+        onKeyDown={onKeyDown}
+        onFocus={onFocus}
+        label={label}
+      />
+    </FormControl>
+  );
+};
--- src/pages/admin/profile/Profile.tsx
+++ src/pages/admin/profile/Profile.tsx
@@ -1,19 +1,67 @@
 import './profile.scss';
+import InputLabelEmailChange from '../../../components/inputs/InputLabelEmailChange.tsx';
+import { InputLabelPasswordClean } from '../../../components/inputs/InputLabelPasswordClean.tsx';
+import { InputPhoneNumberDisabled } from '../../../components/inputs/InputPhoneNumberDisabled.tsx';
 
 function Profile() {
   return (
     <div className="profile">
       <h1>Profile</h1>
       <div className="userInfo">
-        <img src={'https://i.pinimg.com/originals/78/73/70/787370e61c34dfbfb798ce08ac75a610.png'} className="avatar" />
+        <img
+          src={'https://i.pinimg.com/originals/78/73/70/787370e61c34dfbfb798ce08ac75a610.png'}
+          className="avatar"
+          alt="avatar"
+        />
         <div className="infoContainer">
           <div className="fioContainer">
             <div className="fio">Daniil Petrov</div>
           </div>
           <div className="loginCase">
             <div className="log">dpetrov</div>
-            <img src="/edit.svg" className="edit-btn" />
+            <img src="/edit.svg" className="edit-btn" alt="edit-button" />
           </div>
+        </div>
+      </div>
+      <div className="info-change">
+        <div className="inputs">
+          <InputLabelEmailChange
+            disabled={true}
+            error={false}
+            label={'Email'}
+            size={'medium'}
+            value={'daniilpietrov00@gmail.com'}
+            style={{ width: '100%', maxWidth: '510px', minWidth: '320px', maxHeight: '56px' }}
+            onChange={(e) => console.log(e)}
+            onKeyDown={(e) => console.log(e)}
+            onFocus={(e) => console.log(e)}
+            onClick={(e) => console.log(e)}
+          />
+          <div className="pass-cnt">
+            <InputLabelPasswordClean
+              disabled={true}
+              label={'Password'}
+              value={'password'}
+              style={{ width: '100%', maxWidth: '510px', minWidth: '320px', maxHeight: '56px' }}
+              size={'medium'}
+              onChange={(e) => console.log(e)}
+              onKeyDown={(e) => console.log(e)}
+              onFocus={(e) => console.log(e)}
+              onClick={(e) => console.log(e)}
+            />
+            <div className="change-password">Change password</div>
+          </div>
+          <InputPhoneNumberDisabled
+            disabled={true}
+            label={'Phone number'}
+            value={'79135954112'}
+            style={{ width: '100%', maxWidth: '510px', minWidth: '320px', maxHeight: '56px' }}
+            size={'medium'}
+            onChange={(e) => console.log(e)}
+            onKeyDown={(e) => console.log(e)}
+            onFocus={(e) => console.log(e)}
+            onClick={(e) => console.log(e)}
+          />
         </div>
       </div>
     </div>
