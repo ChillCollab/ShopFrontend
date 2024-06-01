f8176dca1d43d2e4916fcabd3a8b27182ef04bcf
Daniil Petrov <dpetrov@msoftgroup.ru>
2024-06-02 00:51:23 +0300

    fix: fix build errors

--- src/pages/admin/home/Home.utils.ts
+++ src/pages/admin/home/Home.utils.ts
@@ -1,20 +1,18 @@
-import authRequests from '../../auth/requests/auth';
-
-export async function getAuth() {
-  try {
-    const userResponse = await authRequests.userInfo();
-    if (userResponse?.status !== 200 || userResponse.data.role <= 0) {
-      const refreshResponse = await authRequests.refreshToken();
-      if (refreshResponse?.status === 200) {
-        localStorage.setItem('access_token', refreshResponse?.data.access_token);
-        localStorage.setItem('refresh_token', refreshResponse?.data.refresh_token);
-        return await authRequests.userInfo();
-      } else return await authRequests.userInfo();
-    } else {
-      return userResponse;
-    }
-  } catch (error) {
-    console.error('Error during authentication:', error);
-    return false;
-  }
-}
+// export async function getAuth() {
+//   try {
+//     const userResponse = await authRequests.userInfo();
+//     if (userResponse?.status !== 200 || userResponse.data.role <= 0) {
+//       const refreshResponse = await authRequests.refreshToken();
+//       if (refreshResponse?.status === 200) {
+//         localStorage.setItem('access_token', refreshResponse?.data.access_token);
+//         localStorage.setItem('refresh_token', refreshResponse?.data.refresh_token);
+//         return await authRequests.userInfo();
+//       } else return await authRequests.userInfo();
+//     } else {
+//       return userResponse;
+//     }
+//   } catch (error) {
+//     console.error('Error during authentication:', error);
+//     return false;
+//   }
+// }
